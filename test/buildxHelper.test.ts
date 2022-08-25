c373833216be599da9d82491dbda19dfb29f98ee
Chenyt26 <2396164919@qq.com>
2022-08-25 09:53:45 +0800

    [Description:]  buildxHelper测试用例重构
    Match-id-6dcfe300491c42f870d7526beb86761588ea078a

--- test/buildxHelper.test.ts
+++ test/buildxHelper.test.ts
@@ -6,82 +6,6 @@ import * as core from '@actions/core';
 import * as toolCache from '@actions/tool-cache'
 import * as fs from 'fs-extra'
 
-// test('check os,platform,arch', async() => {
-//     const osArch = utils.getOSArch();
-//     const osPlatform = utils.getOSPlatform();
-//     await getDownloadURL(osArch,osPlatform);
-//   })
-    
-//   test("check MacOS x86-64 platform buildx download", async() => {
-//     const osArch = "x64";
-//     const osPlatform = "darwin";
-//     expect(await getDownloadURL(osArch,osPlatform)).toEqual("https://github.com/docker/buildx/releases/download/v0.8.2/buildx-v0.8.2.darwin-amd64");
-//   })
-  
-//   test("check MacOS arm-64 platform buildx download", async() => {
-//     const osArch = "arm64";
-//     const osPlatform = "darwin";
-//     expect(await getDownloadURL(osArch,osPlatform)).toEqual("https://github.com/docker/buildx/releases/download/v0.8.2/buildx-v0.8.2.darwin-arm64");
-//   })
-  
-    
-//   test("check windows x86-64 platform buildx download", async() => {
-//     const osArch = "x64";
-//     const osPlatform = "win32";
-//     expect(await getDownloadURL(osArch,osPlatform)).toEqual("https://github.com/docker/buildx/releases/download/v0.8.2/buildx-v0.8.2.windows-amd64.exe");
-//   })
-  
-//   test("check windows arm-64 platform buildx download", async() => {
-//     const osArch = "arm64";
-//     const osPlatform = "win32";
-//     expect(await getDownloadURL(osArch,osPlatform)).toEqual("https://github.com/docker/buildx/releases/download/v0.8.2/buildx-v0.8.2.windows-arm64.exe");
-//   })
-  
-  
-  
-//   test("check Linux x86-64 platform buildx download", async() => {
-//     const osArch = "x64";
-//     const osPlatform = "linux";
-//     expect(await getDownloadURL(osArch,osPlatform)).toEqual("https://github.com/docker/buildx/releases/download/v0.8.2/buildx-v0.8.2.linux-amd64");
-//   })
-  
-//   test("check Linux arm-64 platform buildx download", async() => {
-//     const osArch = "arm64";
-//     const osPlatform = "linux";
-//     expect(await getDownloadURL(osArch,osPlatform)).toEqual("https://github.com/docker/buildx/releases/download/v0.8.2/buildx-v0.8.2.linux-arm64");
-  
-//   })
-  
-  
-//   test("check Linux arm-64 platform buildx download", async() => {
-//     const osArch = "s390x";
-//     const osPlatform = "linux";
-//     expect(await getDownloadURL(osArch,osPlatform)).toEqual("https://github.com/docker/buildx/releases/download/v0.8.2/buildx-v0.8.2.linux-s390x");
-  
-//   })
-  
-//   test("check Linux arm-64 platform buildx download", async() => {
-//     const osArch = "ppc64";
-//     const osPlatform = "linux";
-//     expect(await getDownloadURL(osArch,osPlatform)).toEqual("https://github.com/docker/buildx/releases/download/v0.8.2/buildx-v0.8.2.linux-ppc64le");
-  
-//   })
-  
-//   test("check Linux arm-64 platform buildx download", async() => {
-//     const osArch = "riscv64";
-//     const osPlatform = "linux";
-//     expect(await getDownloadURL(osArch,osPlatform)).toEqual("https://github.com/docker/buildx/releases/download/v0.8.2/buildx-v0.8.2.linux-riscv64");
-  
-//   })
-  
-//   test("check Linux arm-64 platform buildx download", async() => {
-//     const osArch = "arm-v6";
-//     const osPlatform = "linux";
-//     expect(await getDownloadURL(osArch,osPlatform)).toEqual("https://github.com/docker/buildx/releases/download/v0.8.2/buildx-v0.8.2.linux-arm-v6");
-  
-//   })
-
-
 jest.mock('@actions/exec');
 jest.mock('@actions/core');
 jest.mock('@actions/tool-cache');
@@ -98,16 +22,14 @@ describe('check buildx need update', () => {
   });
 
   test('check buildx need update when uselatestbuildx is true and compare version is low', async () => {
-    jest.spyOn(toolCache, 'downloadTool').mockReturnValue( Promise.resolve('\{\n "tag_name": "v0.9.1",\n"target_commitish": "master",\n"name": "v0.9.1"\n}'));
-    jest.spyOn(fs, 'readFileSync').mockReturnValue('\{\n "tag_name": "v0.9.1",\n"target_commitish": "master",\n"name": "v0.9.1"\n}');
+    jest.spyOn(toolCache, 'downloadTool').mockReturnValue( Promise.resolve('\{\n "tag_name": "v0.0.0",\n"target_commitish": "master",\n"name": "v0.0.0"\n}'));
+    jest.spyOn(fs, 'readFileSync').mockReturnValue('\{\n "tag_name": "v9.0.0",\n"target_commitish": "master",\n"name": "v9.0.0"\n}');
     jest.spyOn(exec, 'getExecOutput').mockReturnValue( Promise.resolve(
       {exitCode: 0, stdout: 'github.com/docker/buildx v0.8.2 6224def4dd2c3d347eee19db595348c50d7cb491', stderr: ''}
     ));
     const inputs = {imagetag: '', platforms: '', uselatestbuildx: true, push: true, file: ''};
     expect(await buildx.checkBuildxNeedUpdate(inputs)).toBe(true);
   });
-
-
 });
 
 describe('test install or update docker buildx', () => {
@@ -146,9 +68,9 @@ describe('test install or update docker buildx', () => {
     jest.spyOn(utils, 'getOSArch4Buildx').mockReturnValue('amd64');
     jest.spyOn(utils, 'getOSPlatform4Buildx').mockReturnValue('linux');
     jest.spyOn(toolCache, 'downloadTool')
-      .mockReturnValue(Promise.resolve('\{\n "tag_name": "v0.9.1",\n"target_commitish": "master",\n"name": "v0.9.1"\n}'))
+      .mockReturnValue(Promise.resolve('\{\n "tag_name": "v0.0.0",\n"target_commitish": "master",\n"name": "v0.0.0"\n}'))
       .mockReturnValue(Promise.resolve(''));
-    jest.spyOn(fs, 'readFileSync').mockReturnValue('\{\n "tag_name": "v0.9.1",\n"target_commitish": "master",\n"name": "v0.9.1"\n}');
+    jest.spyOn(fs, 'readFileSync').mockReturnValue('\{\n "tag_name": "v0.0.0",\n"target_commitish": "master",\n"name": "v0.0.0"\n}');
     await buildx.installOrUpdateDockerBuildX();
     expect(core.info).toHaveBeenNthCalledWith(7, 'download docker buildx failed');
   });
@@ -160,13 +82,13 @@ describe('test install or update docker buildx', () => {
     jest.spyOn(utils, 'getOSArch4Buildx').mockReturnValue('amd64');
     jest.spyOn(utils, 'getOSPlatform4Buildx').mockReturnValue('linux');
     jest.spyOn(toolCache, 'downloadTool')
-      .mockReturnValue(Promise.resolve('\{\n "tag_name": "v0.9.1",\n"target_commitish": "master",\n"name": "v0.9.1"\n}'))
-      .mockReturnValue(Promise.resolve('/temp/buildx/v0.9.1'));
-    jest.spyOn(fs, 'readFileSync').mockReturnValue('\{\n "tag_name": "v0.9.1",\n"target_commitish": "master",\n"name": "v0.9.1"\n}');
+      .mockReturnValue(Promise.resolve('\{\n "tag_name": "v0.0.0",\n"target_commitish": "master",\n"name": "v0.0.0"\n}'))
+      .mockReturnValue(Promise.resolve('/temp/buildx/v0.0.0'));
+    jest.spyOn(fs, 'readFileSync').mockReturnValue('\{\n "tag_name": "v0.0.0",\n"target_commitish": "master",\n"name": "v0.0.0"\n}');
     jest.spyOn(utils, 'execCommand').mockReturnValue(Promise.resolve(''));
     jest.spyOn(fs, 'rmSync');
     jest.spyOn(exec, 'getExecOutput').mockReturnValue( Promise.resolve(
-      {exitCode: 0, stdout: 'github.com/docker/buildx v0.9.1 6224def4dd2c3d347eee19db595348c50d7cb491', stderr: ''}
+      {exitCode: 0, stdout: 'github.com/docker/buildx v0.0.0 6224def4dd2c3d347eee19db595348c50d7cb491', stderr: ''}
     ));
     await buildx.installOrUpdateDockerBuildX();
     expect(fs.rmSync).toHaveBeenCalledTimes(1);
@@ -179,19 +101,19 @@ describe('test get buildx download path', () => {
   });
 
   test('check get buildx download path', async () => {
-    jest.spyOn(toolCache, 'downloadTool').mockReturnValue(Promise.resolve('/temp/buildx/v0.9.1'));
-    expect(await buildx.getBuildXDownlodPath('https://xxx.xxx.xxx/v0.9.1/buildx-v0.9.1.linux-amd64')).toBe('/temp/buildx/v0.9.1');
-    expect(core.info).toHaveBeenNthCalledWith(1, 'download docker buildx for install or update from https://xxx.xxx.xxx/v0.9.1/buildx-v0.9.1.linux-amd64');
-    expect(core.info).toHaveBeenNthCalledWith(2, '/temp/buildx/v0.9.1');
+    jest.spyOn(toolCache, 'downloadTool').mockReturnValue(Promise.resolve('/temp/buildx/v0.0.0'));
+    expect(await buildx.getBuildXDownlodPath('https://xxx.xxx.xxx/v0.0.0/buildx-v0.0.0.linux-amd64')).toBe('/temp/buildx/v0.0.0');
+    expect(core.info).toHaveBeenNthCalledWith(1, 'download docker buildx for install or update from https://xxx.xxx.xxx/v0.0.0/buildx-v0.0.0.linux-amd64');
+    expect(core.info).toHaveBeenNthCalledWith(2, '/temp/buildx/v0.0.0');
   });
 
   test('check get buildx download path when throw error', async () => {
     jest.spyOn(toolCache, 'downloadTool').mockImplementation(() => {
       throw new Error('Server Error.');
     });
-    expect(await buildx.getBuildXDownlodPath('https://xxx.xxx.xxx/v0.9.1/buildx-v0.9.1.linux-amd64')).toBe('');
-    expect(core.info).toHaveBeenNthCalledWith(1, 'download docker buildx for install or update from https://xxx.xxx.xxx/v0.9.1/buildx-v0.9.1.linux-amd64');
-    expect(core.info).toHaveBeenNthCalledWith(2, 'Failed to download docker buildx from https://xxx.xxx.xxx/v0.9.1/buildx-v0.9.1.linux-amd64 error info Error: Server Error.');
+    expect(await buildx.getBuildXDownlodPath('https://xxx.xxx.xxx/v0.0.0/buildx-v0.0.0.linux-amd64')).toBe('');
+    expect(core.info).toHaveBeenNthCalledWith(1, 'download docker buildx for install or update from https://xxx.xxx.xxx/v0.0.0/buildx-v0.0.0.linux-amd64');
+    expect(core.info).toHaveBeenNthCalledWith(2, 'Failed to download docker buildx from https://xxx.xxx.xxx/v0.0.0/buildx-v0.0.0.linux-amd64 error info Error: Server Error.');
     expect(core.info).toHaveBeenNthCalledWith(3, '');
   });
   
@@ -203,28 +125,41 @@ describe('test get latest buildx tag', () => {
   });
 
   test('check get latest buildx tag', async () => {
-    jest.spyOn(toolCache, 'downloadTool').mockReturnValue( Promise.resolve('\{\n "tag_name": "v0.9.1",\n"target_commitish": "master",\n"name": "v0.9.1"\n}'));
-    jest.spyOn(fs, 'readFileSync').mockReturnValue('\{\n "tag_name": "v0.9.1",\n"target_commitish": "master",\n"name": "v0.9.1"\n}');
-    expect(await buildx.getLatestBuildxTag()).toBe('v0.9.1');
-    expect(core.info).toHaveBeenNthCalledWith(3, 'latest buildx tagname v0.9.1');
+    jest.spyOn(toolCache, 'downloadTool').mockReturnValue( Promise.resolve('\{\n "tag_name": "v0.0.0",\n"target_commitish": "master",\n"name": "v0.0.0"\n}'));
+    jest.spyOn(fs, 'readFileSync').mockReturnValue('\{\n "tag_name": "v0.0.0",\n"target_commitish": "master",\n"name": "v0.0.0"\n}');
+    expect(await buildx.getLatestBuildxTag()).toBe('v0.0.0');
+    expect(core.info).toHaveBeenNthCalledWith(3, 'latest buildx tagname v0.0.0');
   });
 
   test('check get latest buildx tag when tag_name does not exist', async () => {
-    jest.spyOn(toolCache, 'downloadTool').mockReturnValue( Promise.resolve('\{\n "tag_name": "v0.9.1",\n"target_commitish": "master",\n"name": "v0.9.1"\n}'));
-    jest.spyOn(fs, 'readFileSync').mockReturnValue('\{\n"target_commitish": "master",\n"name": "v0.9.1"\n}');
+    jest.spyOn(toolCache, 'downloadTool').mockReturnValue( Promise.resolve('\{\n "tag_name": "v0.0.0",\n"target_commitish": "master",\n"name": "v0.0.0"\n}'));
+    jest.spyOn(fs, 'readFileSync').mockReturnValue('\{\n"target_commitish": "master",\n"name": "v0.0.0"\n}');
     expect(await buildx.getLatestBuildxTag()).toBe('v0.8.2');
   });
   
 });
 
+
 describe('test get docker buildx download url', () => {
-  test('test get docker buildx download url', async () => {
-    const buildTag = 'v0.9.1';
-    const osPlatform = 'linux';
-    const osArch = 'amd64';
-    const result = 'https://github.com/docker/buildx/releases/download/v0.9.1/buildx-v0.9.1.linux-amd64'
-    expect(buildx.getDockerBuildxDownloadUrl(buildTag, osPlatform, osArch)).toBe(result);
-    expect(core.info).toHaveBeenNthCalledWith(1, `download buildx version : v0.9.1 for current amd64 platform linux,download url ${result}`);
+  const testCase = [
+      { description: 'get docker buildx download url when platfornm is linux,os arch is amd64 and tag name is v0.0.0', buildTag: 'v0.0.0', osPlatform: 'linux', osArch: 'amd64', result: 'https://github.com/docker/buildx/releases/download/v0.0.0/buildx-v0.0.0.linux-amd64'},
+      { description: 'get docker buildx download url when platfornm is linux,os arch is arm-v6 and tag name is v0.0.0', buildTag: 'v0.0.0', osPlatform: 'linux', osArch: 'arm-v6', result: 'https://github.com/docker/buildx/releases/download/v0.0.0/buildx-v0.0.0.linux-arm-v6'},
+      { description: 'get docker buildx download url when platfornm is linux,os arch is riscv64 and tag name is v0.0.0', buildTag: 'v0.0.0', osPlatform: 'linux', osArch: 'riscv64', result: 'https://github.com/docker/buildx/releases/download/v0.0.0/buildx-v0.0.0.linux-riscv64'},
+      { description: 'get docker buildx download url when platfornm is linux,os arch is ppc64 and tag name is v0.0.0', buildTag: 'v0.0.0', osPlatform: 'linux', osArch: 'ppc64', result: 'https://github.com/docker/buildx/releases/download/v0.0.0/buildx-v0.0.0.linux-ppc64'},
+      { description: 'get docker buildx download url when platfornm is linux,os arch is s390x and tag name is v0.0.0', buildTag: 'v0.0.0', osPlatform: 'linux', osArch: 's390x', result: 'https://github.com/docker/buildx/releases/download/v0.0.0/buildx-v0.0.0.linux-s390x'},
+      { description: 'get docker buildx download url when platfornm is linux,os arch is arm64 and tag name is v0.0.0', buildTag: 'v0.0.0', osPlatform: 'linux', osArch: 'arm64', result: 'https://github.com/docker/buildx/releases/download/v0.0.0/buildx-v0.0.0.linux-arm64'},
+      { description: 'get docker buildx download url when platfornm is linux,os arch is x64 and tag name is v0.0.0', buildTag: 'v0.0.0', osPlatform: 'linux', osArch: 'x64', result: 'https://github.com/docker/buildx/releases/download/v0.0.0/buildx-v0.0.0.linux-x64'},
+      { description: 'get docker buildx download url when platfornm is win32,os arch is arm64 and tag name is v0.0.0', buildTag: 'v0.0.0', osPlatform: 'win32', osArch: 'arm64', result: 'https://github.com/docker/buildx/releases/download/v0.0.0/buildx-v0.0.0.win32-arm64'},
+      { description: 'get docker buildx download url when platfornm is win32,os arch is x64 and tag name is v0.0.0', buildTag: 'v0.0.0', osPlatform: 'win32', osArch: 'x64', result: 'https://github.com/docker/buildx/releases/download/v0.0.0/buildx-v0.0.0.win32-x64'},
+      { description: 'get docker buildx download url when platfornm is darwin,os arch is arm64 and tag name is v0.0.0', buildTag: 'v0.0.0', osPlatform: 'darwin', osArch: 'arm64', result: 'https://github.com/docker/buildx/releases/download/v0.0.0/buildx-v0.0.0.darwin-arm64'},
+      { description: 'get docker buildx download url when platfornm is darwin,os arch is x64 and tag name is v0.0.0', buildTag: 'v0.0.0', osPlatform: 'darwin', osArch: 'x64', result: 'https://github.com/docker/buildx/releases/download/v0.0.0/buildx-v0.0.0.darwin-x64'},
+  ];
+  testCase.forEach((item) => {
+      const { description, buildTag, osPlatform, osArch, result } = item;
+      test(`${description}`, async () => {
+        expect(buildx.getDockerBuildxDownloadUrl(buildTag, osPlatform, osArch)).toBe(result);
+        expect(core.info).toHaveBeenNthCalledWith(1, `download buildx version : ${buildTag} for current ${osArch} platform ${osPlatform},download url ${result}`);
+      });
   });
 });
 
